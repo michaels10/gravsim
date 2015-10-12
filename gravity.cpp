028533531f102d69e8b2ea856459dab2205abab7
Michael Sandler <jokegred@gmail.com>
2015-10-11 23:18:32 -0700

    Draw a circle

--- gravity.cpp
+++ gravity.cpp
@@ -1,9 +1,7 @@
-#include "SFML/graphics.hpp"
+#include "gravity.hpp"
+#include "SFML/Graphics.hpp"
 #include <cmath>
-#include "gravity.h"
 
-using sf::Event;
-using sf::Vector2f;
 using sf::Vector2i;
 
 const int WIN_HEIGHT = 480;
@@ -12,24 +10,31 @@ const float ZOOM_FACTOR = 1.2;
 const int SCROLL_AMOUNT = 5;
 const float KEY_ZOOM = 1.0;
 
-int graphicsLoop(const terrain* map) {
+int main() {
     Event event;
     bool drag = false;
     Vector2f refCrsrCoords(-1, -1);
-    
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 8;
+    sf::RenderWindow window(sf::VideoMode(640, 480), "PhysSym", sf::Style::Default, settings);
+    sf::CircleShape shape(50);
+
     while(window.isOpen()){
+        window.clear(sf::Color::Black);
         while (window.pollEvent(event)) {
             if (event.type == Event::Closed)
                 window.close();
             
-            if (event.type == Event::KeyReleased && event.key.code == sf::Keyboard::Key::Q)
+            if (event.type == Event::KeyReleased && event.key.code == sf::Keyboard::Q)
                 window.close();
             
-            keyMove(event);
-            mouseMove(event, drag, refCrsrCoords);                 
+            keyMove(window, event);
+            mouseMove(window, event, drag, refCrsrCoords);                 
         }
-        window.clear(sf::Color::Black);
-        window.draw(sprite);
+
+        shape.setFillColor(sf::Color(100,250,50));
+        shape.setPosition(100,100);
+        window.draw(shape);
         window.display();
     }
     
@@ -78,28 +83,28 @@ void moveWindowByVector(sf::RenderWindow& window, const Vector2f offset) {
     window.setView(view);
 }
 
-void keyMove(Event event){
+void keyMove(sf::RenderWindow& window, Event event){
     float windowScale = (float)(window.getSize().x);
     if (event.type == Event::KeyPressed && event.key.shift) {
-        if (event.key.code == sf::Keyboard::Key::Up)
+        if (event.key.code == sf::Keyboard::Up)
             zoomToCenter(window, KEY_ZOOM);
-        else if (event.key.code == sf::Keyboard::Key::Down)
+        else if (event.key.code == sf::Keyboard::Down)
             zoomToCenter(window, -KEY_ZOOM);
     } else if (event.type == Event::KeyPressed) {
         // TODO scale scroll based on current zoom level
         float windowShift = SCROLL_AMOUNT;//SCROLL_AMOUNT*windowScale/480;
-        if (event.key.code == sf::Keyboard::Key::Left)
-            moveWindowByVector(window, Vector2f{-windowShift, 0});
-        if (event.key.code == sf::Keyboard::Key::Right)
-            moveWindowByVector(window, Vector2f{windowShift, 0});
-        if (event.key.code == sf::Keyboard::Key::Up)
-            moveWindowByVector(window, Vector2f{0, -windowShift});
-        if (event.key.code == sf::Keyboard::Key::Down)
-            moveWindowByVector(window, Vector2f{0, windowShift});
+        if (event.key.code == sf::Keyboard::Left)
+            moveWindowByVector(window, Vector2f(-windowShift, 0));
+        if (event.key.code == sf::Keyboard::Right)
+            moveWindowByVector(window, Vector2f(windowShift, 0));
+        if (event.key.code == sf::Keyboard::Up)
+            moveWindowByVector(window, Vector2f(0, -windowShift));
+        if (event.key.code == sf::Keyboard::Down)
+            moveWindowByVector(window, Vector2f(0, windowShift));
     }
 }
 
-void mouseMove(Event event, bool& drag, Vector2f& refCrsrCoords){ //TODO: Fix badly done parameters. 
+void mouseMove(sf::RenderWindow& window, Event event, bool& drag, Vector2f& refCrsrCoords){ //TODO: Fix badly done parameters. 
     if (event.type == Event::MouseWheelMoved) 
         zoomToMouse(window, event);
         
@@ -119,7 +124,7 @@ void mouseMove(Event event, bool& drag, Vector2f& refCrsrCoords){ //TODO: Fix ba
         Vector2f crsrCoords = window.mapPixelToCoords(crsrPixel);
         float x = (refCrsrCoords.x - crsrCoords.x);
         float y = (refCrsrCoords.y - crsrCoords.y);
-        moveWindowByVector(window, Vector2f{x, y});
+        moveWindowByVector(window, Vector2f(x, y));
     }
 }  
 
